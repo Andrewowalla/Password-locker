bee73060b7fc001d5ecb6e9fc7c7346e0c2a1e43
Andrewowalla <andrewowalla@gmail.com>
2022-01-23 22:47:12 +0300

    create class user

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    '''
+    class that generates new instances of users
+    '''
+    user_list = [] #empty user list
+
+    def __init__(self, username, password):
+        self.userName = userName
+        self.password = password 
+
+    def save_user(self)
+       '''
+       save_user method that saves users in the user list
+       '''
+       User.user_list.append(self)
