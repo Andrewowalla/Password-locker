4f3af674374a136d7b535cad366f992b6b3900bb
Andrewowalla <andrewowalla@gmail.com>
2022-01-25 06:35:05 +0300

    add setup method and test init in test class user

--- user_test.py
+++ user_test.py
@@ -5,5 +5,19 @@ class TestUser(unittest.Testcase):
     '''
     Test class that defines test cases for the user class behaviours
     '''
-
     
+    def setUp(self):
+        '''
+        setup method to run before each test cases
+        '''
+        self.new_user = User("Mazlaowalla", "des0708") #create user object
+
+    def test_init(self):
+        '''
+        test_init to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.userName"Mazlaowalla")
+        self.assertEqual(self.new_user.password"des0708")
+
+if __name__ = '__main__':
+    unittest.main()
