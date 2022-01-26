eb09fe40162d80278a6c144d245414e61d45846d
Andrewowalla <andrewowalla@gmail.com>
2022-01-26 13:19:27 +0300

    test case to save credentials

--- credentials_test.py
+++ credentials_test.py
@@ -5,4 +5,17 @@ class TestCredentials(unittest.TestCase):
         '''
         self.new_credentials = Credentials("Youtube", "Mazlaowalla", "des0708")
 
-    
+    def test_save_credentials(self):
+        '''
+        test case to check if credentials have been added to the credentials list
+        '''
+        self.new_credentials.save_credentials()  # saving the credentials
+        self.assertEqual(len(Credentials.credentials_list), 1)
+
+    def tearDown(self):
+        '''
+        test case that does clean up after every test case
+        '''
+
+        Credentials.credentials_list = []
+
