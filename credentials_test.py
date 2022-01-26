64199174412cdc5a20a13e21e92e2f9271c09819
Andrewowalla <andrewowalla@gmail.com>
2022-01-26 13:18:12 +0300

    test case to run before each test case

--- credentials_test.py
+++ credentials_test.py
@@ -1,1 +1,8 @@
 class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        '''
+        test case to run before each test case
+        '''
+        self.new_credentials = Credentials("Youtube", "Mazlaowalla", "des0708")
+
+    
